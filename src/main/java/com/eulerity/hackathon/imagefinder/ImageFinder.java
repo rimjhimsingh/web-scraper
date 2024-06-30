5a69f8f60c282f11b225d824877d37027bd065ca
Rimjhim Singh <rsingh16@binghamton.edu>
2024-06-30 12:00:00 -0400

    Added sub pages scraping

---
+++ src/main/java/com/eulerity/hackathon/imagefinder/ImageFinder.java
@@ -0,0 +1,83 @@
+package com.eulerity.hackathon.imagefinder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@WebServlet(name = "ImageFinder", urlPatterns = { "/main" })
+public class ImageFinder extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected static final Gson GSON = new GsonBuilder().create();
+	private static final int MAX_DEPTH = 2;
+
+	public static final ArrayList<String> testImages = new ArrayList<>();
+	HashSet<String> visited = new HashSet<>();
+	HashSet<String> imageSet = new HashSet<>(); // To avoid duplicate images
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		resp.setContentType("application/json");
+		String url = req.getParameter("url");
+		String path = req.getServletPath();
+		if (url == null || url.isEmpty()) {
+			resp.getWriter().print("");
+		}
+		testImages.clear();
+		imageSet.clear();
+		visited.add(url);
+		System.out.println("Got request at:" + path + " with URL parameter:" + url);
+
+		crawl(url, 1, visited, testImages, imageSet);
+		resp.getWriter().print(GSON.toJson(testImages));
+
+	}
+
+	private static void crawl(String url, int depth, HashSet<String> visited, ArrayList<String> testImages,
+			HashSet<String> imageSet)
+			throws IOException {
+		if (depth > MAX_DEPTH) {
+			return;
+		}
+
+		try {
+			Document doc = Jsoup.connect(url)
+					.userAgent(
+							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+					.get();
+			visited.add(url);
+			Elements imageElements = doc.select("img[src]");
+			for (Element image : imageElements) {
+				String imageUrl = image.attr("abs:src");
+				if (imageSet.add(imageUrl)) {
+					testImages.add(imageUrl);
+					System.out.println(imageUrl);
+				}
+			}
+
+			Elements links = doc.select("a[href]");
+			for (Element link : links) {
+				String nextLink = link.absUrl("href");
+				if (!visited.contains(nextLink) && nextLink.startsWith("http")) {
+					crawl(nextLink, depth + 1, visited, testImages, imageSet);
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to retrieve content from " + url);
+			e.printStackTrace();
+		}
+	}
+}
